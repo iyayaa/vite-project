0af18cc7460d3913caf00e14a532ee5d1d3c46f9
zhangyilin <niliyzhang@foxmail.com>
2022-05-30 17:50:28 +0800

    feature: 添加pinia

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,37 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        //设置别名
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    base: './', // 设置公共基础路径
-    build: {
-        outDir: '/dist', // 指定打包路径，默认为项目根目录下的 dist 目录
-        terserOptions: {
-            compress: {
-                keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
-                drop_console: true, // 生产环境去除 console
-                drop_debugger: true // 生产环境去除 debugger
-            }
-        },
-        chunkSizeWarningLimit: 1500 // chunk 大小警告的限制（以 kbs 为单位）
-    },
-    server: {
-        port: 8080, //启动端口
-        open: true, // 服务启动时是否自动打开浏览器
-        cors: true, // 允许跨域
-        // 设置代理
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1:8000',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path: string) => path.replace(/^\/api/, '')
-            }
-        }
+  plugins: [vue()],
+  resolve: {
+    //设置别名
+    alias: {
+      '@': path.resolve(__dirname, 'src')
     }
+  },
+  base: './', // 设置公共基础路径
+  build: {
+    outDir: '/dist', // 指定打包路径，默认为项目根目录下的 dist 目录
+    terserOptions: {
+      compress: {
+        keep_infinity: true, // 防止 Infinity 被压缩成 1/0，这可能会导致 Chrome 上的性能问题
+        drop_console: true, // 生产环境去除 console
+        drop_debugger: true // 生产环境去除 debugger
+      }
+    },
+    chunkSizeWarningLimit: 1500 // chunk 大小警告的限制（以 kbs 为单位）
+  },
+  server: {
+    port: 8080, //启动端口
+    open: true, // 服务启动时是否自动打开浏览器
+    cors: true, // 允许跨域
+    // 设置代理
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path: string) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
